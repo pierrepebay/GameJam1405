0cdf84bba3eeac2ddd490863ca1462ee78775016
Taha Belkhiri <belkhiri.taha53@gmail.com>
2022-05-14 12:29:35 +0200

    added map

---
+++ src/map.c
@@ -0,0 +1,37 @@
+#include "map.h"
+
+int map[MAP_H][MAP_W];
+
+void initMap(){
+    int i;
+    int j;
+    for (i=0; i<MAP_H; ++i){
+        for (j=0; j<MAP_W; ++j){
+            map[i][j] = 1;
+        }
+    }
+    for (i=MAP_H/2; i < MAP_H; ++i){
+        for (j= 0; j < MAP_W; ++j){
+            map[i][j] = 1;
+        }
+    }
+    for (int i=0; i < MAP_H; i++){
+        map[i][0] = 1;
+        map[i][1] = 1;
+        map[i][MAP_W-2] = 1;
+        map[i][MAP_W-1] = 1;
+    }
+    for (int i = 0; i < MAP_W; i++){
+        map[0][i] = 1;
+        map[1][i] = 1;
+    }
+}
+
+void printMap(){
+    for (int i = 0; i < MAP_H; i++){
+        for (int j = 0; j < MAP_W; j++){
+            printf("%d", map[i][j]);
+        }
+        printf("\n");
+    }
+}
---
+++ src/map.h
@@ -0,0 +1,17 @@
+#ifndef _MAP_H_
+#define _MAP_H_
+
+#define MAP_W  200
+#define MAP_H  20
+
+#include <stdio.h>
+
+extern int map[MAP_H][MAP_W];
+
+
+void initMap();
+
+void printMap();
+
+
+#endif
